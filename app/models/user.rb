d3937cdbd2a881bb68178af46437f612a2118597
andreas kundig <andreas.kuendig@gmx.ch>
2023-12-27 19:52:25 +0100

    example model test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
   has_many :user_reminders
   has_many :reminders, through: :user_reminders
+  validates :email, presence: true
 end
--- test/models/reminder_test.rb
+++ test/models/reminder_test.rb
@@ -1,7 +1,8 @@
 require "test_helper"
 
 class ReminderTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save reminder without date" do
+    reminder = Reminder.new
+    assert_not reminder.save, 'Saved reminder without title'
+  end
 end
