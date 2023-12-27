446d5e78eb404ab30d0499aa729b26917da4c843
andreas kundig <andreas.kuendig@gmx.ch>
2023-12-27 13:56:15 +0100

    new and create reminders

--- app/controllers/reminders_controller.rb
+++ app/controllers/reminders_controller.rb
@@ -7,4 +7,26 @@ class RemindersController < ApplicationController
     @reminder = Reminder.find(params[:id])
   end
 
+  def new
+    @reminder = Reminder.new
+  end
+
+  def create
+    @reminder = Reminder.new(reminder_params)
+
+    if @reminder.save # saves to db
+      # save has worked
+      # makes a new request to end this one
+      redirect_to @reminder
+    else
+      # just renders the view new.html.erb,
+      # with an error, and without a new request
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def reminder_params
+      params.require(:reminder).permit(:date)
+    end
 end
--- app/models/reminder.rb
+++ app/models/reminder.rb
@@ -1,4 +1,5 @@
 class Reminder < ApplicationRecord
+  validates :date, presence: true
   has_many :user_reminders
   has_many :users, through: :user_reminders
 end
