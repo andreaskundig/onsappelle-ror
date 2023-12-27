10d11566ea5844566b60283b40a6d49aa1c523bc
andreas kundig <andreas.kuendig@gmx.ch>
2023-12-27 14:09:37 +0100

    update and delete reminders

--- app/controllers/reminders_controller.rb
+++ app/controllers/reminders_controller.rb
@@ -25,6 +25,27 @@ class RemindersController < ApplicationController
     end
   end
 
+  def edit
+    @reminder = Reminder.find(params[:id])
+  end
+
+  def update
+    @reminder = Reminder.find(params[:id])
+
+    if @reminder.update(reminder_params)
+      redirect_to @reminder
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @reminder = Reminder.find(params[:id])
+    @reminder.destroy
+
+    redirect_to root_path, status: :see_other
+  end
+
   private
     def reminder_params
       params.require(:reminder).permit(:date)
