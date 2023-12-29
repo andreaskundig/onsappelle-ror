5e1a1d1e59901923fe359c3999e5434c2a925638
andreas kundig <andreas.kuendig@gmx.ch>
2023-12-29 05:13:57 +0100

    separate user crud paths

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,25 @@
 class UsersController < ApplicationController
   include ReminderFactory
 
+  def index
+    @reminder = Reminder.find(params[:reminder_id])
+  end
+
+  def show
+    @reminder = Reminder.find(params[:reminder_id])
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @reminder = Reminder.find(params[:reminder_id])
+    # @user = @reminder.users.build
+  end
+
   def create
     @reminder = Reminder.find(params[:reminder_id])
     email = params[:user][:email]
     add_reminder_recipients(@reminder, [email])
+    @reminder.save
     redirect_to reminder_path(@reminder)
   end
 
