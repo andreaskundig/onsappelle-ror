f4407bae876bc4430b7e1c2fa6d7f1ab9bb6ee98
andreas kundig <andreas.kuendig@gmx.ch>
2024-01-03 01:58:19 +0100

    fix removing users at update

--- app/controllers/concerns/reminder_factory.rb
+++ app/controllers/concerns/reminder_factory.rb
@@ -15,6 +15,12 @@ module ReminderFactory
 
   def add_reminder_recipients(reminder, user_params)
     emails = user_params&.map {|u| u[:email]}
+    return unless emails
+    reminder.users.each do |user|
+      unless emails.include? user.email
+        reminder.users.delete(user)
+      end
+    end
     emails&.each{ |email|
       invalid = (email.nil? or email.empty?)
       unless invalid or reminder_has_email(reminder, email)
