1817aca4a456982a654d70f1e587a5ca4fa18ecb
andreas kundig <andreas.kuendig@gmx.ch>
2024-01-03 08:38:21 +0100

    reminder update test

--- test/system/reminders_test.rb
+++ test/system/reminders_test.rb
@@ -10,7 +10,6 @@ class RemindersTest < ApplicationSystemTestCase
     assert_selector "h1", text: "Reminders"
   end
 
-
   test "can create a reminder" do
     reminder_count = Reminder.count
     user_count = User.count
@@ -53,5 +52,43 @@ class RemindersTest < ApplicationSystemTestCase
     assert_equal DateTime.new(2023, 1, 4), new_reminder.date
     assert_equal email1, new_reminder.users[0].email
     assert_equal email2, new_reminder.users[1].email
+  end
+
+  test "can update a reminder" do
+    reminder = Reminder.last
+    user_count = reminder.users.count
+    assert_equal 1, user_count
+    user1 =  reminder.users[0]
+    email1 = user1.email
+    email2 = "update2@reminder.test"
+    email3 = "update3@reminder.test"
+
+    visit reminder_path(reminder)
+    assert_selector "h1", text: "Reminder #{reminder.id}"
+    assert_selector "span", text: email1
+    assert_no_selector "span", text: email2
+    assert_no_selector "span", text: email3
+
+    fill_in "user_email", with: email2
+    click_on "Add recipient"
+    assert_selector "span", text: email2
+
+    fill_in "user_email", with: email3
+    click_on "Add recipient"
+    assert_selector "span", text: email3
+
+
+    assert_selector "a[href$='#{email_to_code(email1)}']", text: '[-]'
+
+    find("a[href$='#{email_to_code(email1)}']").click
+    assert_no_selector "span", text: email1
+
+    click_on "Update reminder"
+
+    reminder.reload
+    assert_equal user_count + 1, reminder.users.count
+
+    assert_equal email2, reminder.users[0].email
+    assert_equal email3, reminder.users[1].email
   end
 end
