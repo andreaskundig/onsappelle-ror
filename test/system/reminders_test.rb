0c9c04f8f59cf0d805c63bfe0cde4afd6f0855fa
andreas kundig <andreas.kuendig@gmx.ch>
2024-01-19 19:00:56 +0100

    try to test an email...

--- test/system/reminders_test.rb
+++ test/system/reminders_test.rb
@@ -1,8 +1,24 @@
 require "application_system_test_case"
 
+# Prevent test helper from overriding mail delivery method
+# https://stackoverflow.com/questions/57776074/rails-6-deliver-later-doesnt-affect-actionmailerbase-deliveries
+module ActiveJob
+  module TestHelper
+    def before_setup
+      super
+    end
+  end
+end
 
 class RemindersTest < ApplicationSystemTestCase
   include UsersHelper
+  include ActionMailer::TestHelper
+
+  # failing workaround
+    # https://github.com/rails/rails/issues/37270
+  # def setup
+  #     (ActiveJob::Base.descendants << ActiveJob::Base).each(&:disable_test_adapter)
+  # end
 
   test "visiting the index" do
     visit reminders_url
@@ -13,9 +29,10 @@ class RemindersTest < ApplicationSystemTestCase
   test "can create a reminder" do
     reminder_count = Reminder.count
     user_count = User.count
-    email1 = "create1@reminder.test"
-    email2 = "create2@reminder.test"
-    email3 = "create3@reminder.test"
+    p = Time.now.strftime("%H-%M-%S-%L-")
+    email1 = p+"create1@reminder.test"
+    email2 = p+"create2@reminder.test"
+    email3 = p+"create3@reminder.test"
 
 
     visit new_reminder_path
@@ -43,13 +60,31 @@ class RemindersTest < ApplicationSystemTestCase
     assert_equal reminder_count, Reminder.count
     assert_equal user_count, User.count
 
-    click_on "Create reminder"
+    assert File.exist?("tmp/mails/12-11-39-332-create1@reminder.test")
+
+    assert !File.exist?("tmp/mails/#{email1}")
+    # a bug breaks assert_emails
+    # https://github.com/rails/rails/issues/37270
+    # https://stackoverflow.com/questions/57776074/rails-6-deliver-later-doesnt-affect-actionmailerbase-deliveries
+    # perform_enqueued_jobs do
+    # assert_emails 1 do
+    #     click_on "Create reminder"
+    # end
+    # end
+    perform_enqueued_jobs do
+      click_on "Create reminder"
+    end
+    until File.exist?("tmp/mails/#{email1}")
+        sleep(5)
+    end
+    assert File.exist?("tmp/mails/#{email1}")
 
     assert_equal reminder_count + 1, Reminder.count
     assert_equal user_count + 2, User.count
 
     new_reminder = Reminder.last
     assert_equal DateTime.new(2023, 1, 4), new_reminder.date
+    assert_equal 2, new_reminder.users.length
     assert_equal email1, new_reminder.users[0].email
     assert_equal email2, new_reminder.users[1].email
   end
